5db6e3d66a0ab6c39c90fc1e9af7d5ee3c0b1c91
Victor Bathke <contato@victorbathke.eti.br>
2015-01-23 10:59:32 -0200

    First commit

---
+++ setupSeleniumServer.sh
@@ -0,0 +1,5 @@
+cd ~
+wget http://selenium-release.storage.googleapis.com/2.44/selenium-server-standalone-2.44.0.jar
+sudo apt-get update
+sudo apt-get -y install openjdk-7-jre-headless
+nohup java -jar selenium-server-standalone-2.44.0.jar -role hub -port 8081 &
