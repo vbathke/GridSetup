037389a54176039fd90ad1951e520150455eb3bf
Victor Bathke <contato@victorbathke.eti.br>
2015-10-27 00:23:29 -0200

    update

--- dockerhub.sh
+++ dockerhub.sh
@@ -15,3 +15,13 @@ sudo dpkg -i init-system-helpers_1.18_all.deb;
 sudo apt-get --force-yes -y install docker-engine;
 sudo service docker start;
 sudo docker run -d -p 80:4444 --name selenium-hub2 selenium/hub:2.48.2;
+
+if [ $3 != "extern" ]
+then
+        NODEHOST=`ifconfig eth0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}'`;
+else
+        NODEHOST=`curl http://ipecho.net/plain`
+fi
+
+echo "Command to append node:\n";
+echo "curl https://raw.githubusercontent.com/vbathke/GridSetup/master/https://raw.githubusercontent.com/vbathke/GridSetup/master/dockernode.sh $NODEHOST | sh "
